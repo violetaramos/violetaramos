5d8d07b5206704e1d9cef4e9ac6f4c4a1e8d7232
Violeta Ramos <38788703+violetaramos@users.noreply.github.com>
2023-07-05 22:38:05 -0600

    New projects

--- js/data.js
+++ js/data.js
@@ -10,88 +10,76 @@ var console_txt_en = {
    'p3': "C:\134  I have 5 years of experience working in the development and maintenance of websites, e-commerce, information systems (ERP, CRM), landing pages, mailing, website optimization, SEO knowledge, creation of custom themes for WordPress and Shopify , under the use of languages such as PHP, Liquid, JavaScript, CSS, HTML and JQuery.",
    'p4': "C:\134  With the use of technologies such as WordPress, Shopify, Laravel and MyCavi (MVC framework in PHP of the company where I currently work based on OOP)."
 }
-var innovation = {
-   "id": "innovation",            
-   "name": "Innovation Line",
-   "url": "https://innovation.com.mx/",
-   "miniature": "background-image: url(https://violetaramos.github.io/violetaramos/images/projects/innovation_min.jpg);",
+var allux = {
+   "id": "allux",            
+   "name": "Allux",
+   "url": "https://allux.com.mx",
+   "miniature": "background-image: url(https://violetaramos.github.io/violetaramos/images/projects/allux_min.jpg);",
    "images":[
       {
-         "url": "https://violetaramos.github.io/violetaramos/images/projects/innovation.jpg",
-         "name": "innovation.jpg"
+         "url": "https://violetaramos.github.io/violetaramos/images/projects/allux.jpg",
+         "name": "allux.jpg"
       }  
    ],
    "type": {
-      "es": "Sitio E-commerce",
-      "en": "Site E-commerce"
+      "es": "Sitio Web",
+      "en": "Website"
    },
    "extract": {
-      "es": "Desarrollo Frontend de MyCavi",
-      "en": "Frontend development of MyCavi."
+      "es": "Desarrollo Sitio web en WordPress",
+      "en": "Website Development in WordPress"
    },
    "description": {
-      "es": "Desarrollo frontend de MyCavi (MVC framework de Telaio basado en OOP). Con el uso de lenguajes de PHP, JavaScript, CSS, y HTML. Más apoyo de las librerias Bootstrap, AOS y JQuery",
-      "en": "Frontend development of MyCavi (Telaio MVC framework based on OOP). With the use of PHP, JavaScript, CSS, and HTML languages. More support from Bootstrap, AOS and JQuery libraries"
+      "es": "Sitio Web desarrollado en WordPress con tema customizado. Con el uso de los lenguajes PHP, JavaScript, CSS y HTML. Más apoyo de las librerias Bootstrap y JQuery.",
+      "en": "Website developed in WordPress with a custom theme. With the use of PHP, JavaScript, CSS and HTML languages. More support from Bootstrap and JQuery libraries."
    }
 };
-var petronas = {
-   "id": "petronas",            
-   "name": "PETRONAS México",
-   "url": "http://granpremiopetronas.com/",
-   "miniature": "background-image: url(https://violetaramos.github.io/violetaramos/images/projects/petronas_min.jpg);",
+var pridemex = {
+   "id": "pridemex",            
+   "name": "Pridemex",
+   "url": "https://www.pridemex.com",
+   "miniature": "background-image: url(https://violetaramos.github.io/violetaramos/images/projects/pridemex_min.jpg);",
    "images":[
       {
-         "url": "https://violetaramos.github.io/violetaramos/images/projects/granpremiopetronas_site.jpg",
-         "name": "granpremiopetronas_site.jpg"
-      },
-      {
-         "url": "https://violetaramos.github.io/violetaramos/images/projects/intranet.granpremiopetronas.jpg",
-         "name": "intranet.granpremiopetronas.jpg"
-      },
-      {
-         "url": "https://violetaramos.github.io/violetaramos/images/projects/intranet_granpremiopetronas_2.jpg",
-         "name": "intranet_granpremiopetronas_2.jpg"
-      },
-      {
-         "url": "https://violetaramos.github.io/violetaramos/images/projects/intranet_granpremiopetronas_3.jpg",
-         "name": "intranet_granpremiopetronas_3.jpg"
+         "url": "https://violetaramos.github.io/violetaramos/images/projects/pridemex.jpg",
+         "name": "pridemex.jpg"
       }  
    ],
    "type": {
-      "es": "Intranet y Sitio Web",
-      "en": "Intranet & Website"
+      "es": "Administrador y Sitio Web",
+      "en": "Administrator & Website"
    },
    "extract": {
-      "es": "Desarrollo Sitio web en WordPress - Diseño y desarrollo de intranet en MyCavi",
-      "en": "Website Development in WordPress - Intranet design and development in MyCavi"
+      "es": "Desarrollo Sitio web en WordPress y Laravel",
+      "en": "Website Development in WordPress and Laravel"
    },
    "description": {
-      "es": "Sitio Web desarrollado en WordPress con tema customizado. Diseño y desarrollo de Interfase de la Intranet en MyCavi (MVC framework de Telaio basado en OOP). Con el uso de los lenguajes PHP, JavaScript, CSS y HTML. Más apoyo de las librerias Bootstrap y JQuery.",
-      "en": "Website developed in WordPress with a custom theme. Design and development of Intranet Interface in MyCavi (MVC framework of Telaio based on OOP). With the use of PHP, JavaScript, CSS and HTML languages. More support from Bootstrap and JQuery libraries."
+      "es": "Sitio Web desarrollado en WordPress con tema customizado. Diseño y desarrollo de administrador en Laravel. Con el uso de los lenguajes PHP, JavaScript, CSS y HTML.",
+      "en": "Website developed in WordPress with a custom theme. Design and development of Intranet Interface in MyCavi (MVC framework of Telaio based on OOP). With the use of PHP, JavaScript, CSS and HTML languages."
    }
 };
-var geefit = {
-   "id": "geefit",            
-   "name": "Gee Fit Store",
-   "url": "https://gee-fit-store.myshopify.com/",
-   "miniature": "background-image: url(https://violetaramos.github.io/violetaramos/images/projects/geefit_min.jpg);",
+var share = {
+   "id": "share",            
+   "name": "Share",
+   "url": "https://www.sharemanagement.app",
+   "miniature": "background-image: url(https://violetaramos.github.io/violetaramos/images/projects/share_min,jpg.jpg);",
    "images":[
       {
-         "url": "https://violetaramos.github.io/violetaramos/images/projects/gee_fit.jpg",
-         "name": "gee_fit.jpg"
+         "url": "https://violetaramos.github.io/violetaramos/images/projects/share.jpg",
+         "name": "share.jpg"
       }  
    ],
    "type": {
-      "es": "Sitio E-commerce",
-      "en": "Site E-commerce"
+      "es": "Sitio Web",
+      "en": "Website"
    },
    "extract": {
-      "es": "Desarrollo de tema en Shopify",
-      "en": "Development and theme design in Shopify"
+      "es": "Desarrollo Sitio web en WordPress",
+      "en": "Website Development in WordPress"
    },
    "description": {
-      "es": "Desarrollo y apoyo en el diseño de tema en Shopify. Con el uso de lenguajes Liquid,JavaScript, CSS, SASS y HTML. Más apoyo de las librerias Bootstrap, AOS y JQuery, asi como el uso de la API de Shopify.",
-      "en": "Development and theme design in Shopify. With the use of Liquid, JavaScript, CSS, SASS and HTML languages. More support for the Bootstrap, AOS and JQuery libraries, as well as the use of the Shopify API."
+      "es": "Sitio Web desarrollado en WordPress con tema customizado. Con el uso de los lenguajes PHP, JavaScript, CSS y HTML. Más apoyo de las librerias Bootstrap y JQuery.",
+      "en": "Website developed in WordPress with a custom theme. With the use of PHP, JavaScript, CSS and HTML languages. More support from Bootstrap and JQuery libraries."
    }
 };
 var alcoba = {
@@ -227,7 +215,7 @@ const data = {
       },
       "subtitle": {
          "es": "La unión de la pasión e innovación que rompen fronteras.",
-         "en": "The union of passion and innovation that breaks boundaries."
+         "en": "The union of passion and allux that breaks boundaries."
       }
    },
    "about": {
@@ -249,7 +237,7 @@ const data = {
          "es": "Ver más",
          "en": "See more"
       },
-      "projects": [alcoba,cobego,myrightmood,innovation,petronas,geefit]
+      "projects": [allux, pridemex, myrightmood, share, alcoba, cobego]
    }
 };
 
@@ -258,9 +246,9 @@ var allprojects = {
       "es": "Ver sitio",
       "en": "See website"
    },
-   "innovation": innovation,
-   "petronas": petronas,
-   "geefit": geefit,
+   "allux": allux,
+   "pridemex": pridemex,
+   "share": share,
    "alcoba": alcoba,
    "cobego": cobego,
    "myrightmood": myrightmood
